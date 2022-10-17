aa22120edaf44a98123c2ac4ce8712d1eda5129c
jvramos-py <jvramos411@gmail.com>
2022-10-16 21:12:05 -0300

    Second Commit.

--- app/src/main/java/com/example/basic_compose_notions/MainActivity.kt
+++ app/src/main/java/com/example/basic_compose_notions/MainActivity.kt
@@ -3,13 +3,17 @@ package com.example.basic_compose_notions
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.basic_compose_notions.ui.theme.Basic_Compose_NotionsTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +26,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    MyApp()
                 }
             }
         }
@@ -30,14 +34,41 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+private fun MyApp(names: List<String> = listOf("World", "Compose")) {
+    Column(modifier = Modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
+    }
+}
+
+@Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    val isExpanded = remember {
+        mutableStateOf(false)
+    }
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = "Hello,")
+                Text(text = name)
+            }
+            OutlinedButton(
+                onClick = { isExpanded.value = !isExpanded.value }
+            ) {
+               Text (if (isExpanded.value) "Show less" else "Show More")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     Basic_Compose_NotionsTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
